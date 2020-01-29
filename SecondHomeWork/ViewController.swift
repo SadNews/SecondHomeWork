fcab89aad87bab80becb6966e92aa330abd2a981
Andrey Ushakov <sadnews@ya.ru>
2020-01-30 01:27:44 +0300

    Initial Commit

---
+++ SecondHomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondHomeWork
+//
+//  Created by Admin on 30.01.2020.
+//  Copyright © 2020 UshakovAndrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
