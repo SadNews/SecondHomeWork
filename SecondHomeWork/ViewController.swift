a221f009f8123159b555016064cbe1e81b2cd540
Andrey Ushakov <sadnews@ya.ru>
2020-01-30 03:01:57 +0300

    first commit

--- SecondHomeWork/ViewController.swift
+++ SecondHomeWork/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var button: UIButton!
+    private var count = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       
+        redView.layer.cornerRadius = 70
+        redView.alpha = 0.2
+        yellowView.layer.cornerRadius = 70
+        yellowView.alpha = 0.2
+        greenView.layer.cornerRadius = 70
+        greenView.alpha = 0.2
+        
+        
+      
     }
 
 
+    @IBAction func pressButton(_ sender: UIButton) {
+        button.setTitle("NEXT", for: .normal)
+        
+        switch count {
+        case 1:
+            greenView.alpha = 0.2
+            redView.alpha = 1
+            count += 1
+        case 2:
+            redView.alpha = 0.2
+            yellowView.alpha = 1
+            count += 1
+        case 3:
+            yellowView.alpha = 0.2
+            greenView.alpha = 1
+            count = 1
+        default:
+            count = 1
+        }
+        
+    }
+    
 }
 
